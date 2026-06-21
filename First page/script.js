75ee042a83218de8a401563c9a3a8876568e3514
itodoh <emmanuelitodoh7@gmail.com>
2026-06-21 18:52:41 +0100

    Add files via upload

---
+++ First page/script.js
@@ -0,0 +1,12 @@
+
+
+
+
+const menuBtn = document. querySelector("#menu-btn")
+const navbar = document. querySelector("nav")
+
+menuBtn.onclick = () => {
+    navbar.classList.toggle("active");
+};
+
+
